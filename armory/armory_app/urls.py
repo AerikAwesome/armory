f97c80a66efc3e92e5627384fbda9020734655e0
Erik Buter <e.t.buter@student.tudelft.nl>
2016-07-02 13:35:10 +0200

    Implemented post detail page

--- armory/armory_app/urls.py
+++ armory/armory_app/urls.py
@@ -3,9 +3,7 @@ from django.conf.urls import urlfrom . import views
 app_name = 'armory_app'
 urlpatterns = [
     # ex: /
-    url(r'^$', views.index, name='index'),    # ex: /blog/    url(r'^blog/$', views.blog, name='blog'),    # ex: /includes/$    url(r'^includes/(?P<req_file>.+)/$', views.includes, name='includes'),
-    # ex: /armory_app/5/
-    url(r'^(?P<post_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^$', views.index, name='index'),    # ex: /blog/    url(r'^blog/(?P<req>[0-9]+)/$', views.blog, name='blog'),    # ex: /blog/1    url(r'^blog/detail/(?P<req>[0-9]+)/$', views.blog_detail, name='blog_detail'),    # ex: /includes/$    url(r'^includes/(?P<req_file>.+)/$', views.includes, name='includes'),
     # ex: /armory_app/5/comment/
-    url(r'^(?P<post_id>[0-9]+)/comment/$', views.comment, name='comment'),
+ #   url(r'^(?P<post_id>[0-9]+)/comment/$', views.comment, name='comment'),
 ]
--- armory/armory_app/views.py
+++ armory/armory_app/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.shortcuts import render
 from django.http import Http404
 from armory_app.models import Post
 
@@ -7,28 +6,29 @@ from armory_app.models import Post
 def index(request):
     return render(request, 'armory_app/index.html')
 
-def blog(request):
-    latest_post_list = Post.objects.order_by('post_date')[:10]
+def blog(request, req=0):
+    include = 'armory_app/blog/blog.html'
+    start_of_range = int(req)
+    end_of_range = start_of_range + 10
+    latest_post_list = Post.objects.order_by('post_date')[start_of_range:end_of_range]
     context = {
                'latest_post_list': latest_post_list,
     }
-    return render(request, 'armory_app/blog.html', context)
+    return render(request, include, context)
 
 def includes(request, req_file='about.html'):
     include = 'armory_app/includes/%s' % req_file
     return render(request, include)
 
-def post_list(request):
-    posts = Post.objects.order_by('-post_date')[:5]
-    return render(request, 'armory_app/post_list.html', {'posts': posts})
-
-def detail(request, post_id):
+def blog_detail(request, req):
     try:
-        post = Post.objects.get(pk=post_id)
+        post = Post.objects.get(pk=req)
     except Post.DoesNotExist:
         raise Http404("Blog post does not exist")
-    return render(request, 'armory_app/detail.html', {'post': post})
+    return render(request, 'armory_app/blog/detail.html', {'post': post})
 
+
+'''
 def comment(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
     try:
@@ -41,3 +41,4 @@ def comment(request, post_id):
         })
     else:
         return HttpResponse("You're commenting on post %s." % post_id)
+'''
